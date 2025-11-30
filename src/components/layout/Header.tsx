afa499ed4dcdae19a8201a12446d1023cc6eade0
Rune <sungjong5k@naver.com>
2025-12-01 00:04:37 +0900

    style: Adjust header padding.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { CloudMoon } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
-    <header className="pt-8 pb-4 px-6 flex justify-center items-center shrink-0 z-10 sticky top-0">
+    <header className="py-3 flex justify-center items-center shrink-0 z-10 sticky top-0">
       <div className="glass-button rounded-full px-4 py-2 flex items-center gap-2">
         <CloudMoon size={24} className="text-(--primary)" />
         <span
